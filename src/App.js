698bd09efa0f29bf217a06e0196b3de048ba4f1a
jabeermk <jabeermk@gmail.com>
2022-09-14 19:28:55 +0530

    jsx file setup

--- src/App.js
+++ src/App.js
@@ -1,33 +1,77 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { FiSettings } from 'react-icons/fi';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import "./App.css";
 
 const App = () => {
+  const activeMenu=false;
+  
   return (
- <div> <BrowserRouter>
- <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-            <TooltipComponent
-              content="Settings"
-              position="Top"
-            >
+    <div>
+      {" "}
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-1 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
               <button
                 type="button"
-              
-                style={{ background: 'blue', borderRadius: '50%' }}
+                style={{ background: "blue", borderRadius: "50%" }}
                 className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
               >
                 <FiSettings />
               </button>
-
             </TooltipComponent>
-   </div>
- </div>
- </BrowserRouter></div>
-  )
-}
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              SidebarOpen
+            </div>
+          ) : (
+            <div className="w-0 fixed dark:bg-secondary-dark-bg">Sidebarclose</div>
+          )}
+          <div
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              Navbar Navbar Navbar
+            </div>
+          </div>
+          <Routes>
+            {/* Dashboards */}
+            {/* <Route path="/" element="Ecommerce" /> */}
+            <Route path="/ecommerce" element="Ecommerce" />
+            {/* pages */}
+            <Route path="/orders" element="Orders" />
+            <Route path="/employees" element="Employees" />
+            <Route path="/customers" element="Customers" />
+            {/* Apps */}
+            <Route path="/kanban" element="Kanban" />
+            <Route path="/editor" element="Editor" />
+            <Route path="/calender" element="Calender" />
+            <Route path="/color-picker" element="ColorPicker" />
+            {/* Charts */}
+            <Route path="/calender" element="Calender" />
+            <Route path="/calender" element="Calender" />
+            <Route path="/calender" element="Calender" />
+            <Route path="/calender" element="Calender" />
+            <Route path="/line" element="{<Line />}" />
+            <Route path="/area" element="{<Area />}" />
+            <Route path="/bar" element="{<Bar />}" />
+            <Route path="/pie" element="{<Pie />}" />
+            <Route path="/financial" element="{<Financial />}" />
+            <Route path="/color-mapping" element="{<ColorMapping />}" />
+            <Route path="/pyramid" element="{<Pyramid />}" />
+            <Route path="/stacked" element="{<Stacked />}" />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
