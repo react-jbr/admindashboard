d802591aec65b2e8cd17973103f63768b3de13f0
jabeermk <jabeermk@gmail.com>
2022-09-16 18:56:50 +0530

    stacked graph added in Ecommerce

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -21,7 +21,7 @@ const Stacked = ({ width, height }) => {
     <ChartComponent
       width={width}
       height={height}
-      id="chaakshdkjahdachartssddrts"
+      id="charts"
       primaryXAxis={stackedPrimaryXAxis}
       primaryYAxis={stackedPrimaryYAxis}
       chartArea={{ border: { width: 0 } }}
