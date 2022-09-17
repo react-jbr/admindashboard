2199f481fdf247429e9314877cfd2f3c33d9f2e8
jabeermk <jabeermk@gmail.com>
2022-09-17 09:42:19 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ import "./App.css";const App = () => {
             {/* Apps */}
             <Route path="/kanban" element={<Kanban/>} />
             <Route path="/editor" element={<Editor/>} />
-            <Route path="/calender" element={<Calendar/>} />
+            <Route path="/calendar" element={<Calendar/>} />
             <Route path="/color-picker" element={<ColorPicker/>} />
             {/* Charts */}
            
